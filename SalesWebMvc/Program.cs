807a610732ebc78af1d430338ec456bad00ed48a
Lucas Queiroz <lucasestevam365@gmail.com>
2023-11-02 13:04:40 -0300

    Seeding Service

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using SalesWebMvc.Data;
 using SalesWebMvc.Models;
 namespace SalesWebMvc
 {
@@ -24,6 +25,15 @@ namespace SalesWebMvc
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+            }
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var context = services.GetRequiredService<SalesWebMvcContext>();
+                context.Database.EnsureCreated();
+                DbInitializer.Initialize(context);
             }
 
             app.UseHttpsRedirection();
